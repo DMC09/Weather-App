def486bff574fd2cc21414407bc081f29780ccd0
Dharmin <dharminlive12@live.com>
2019-10-23 21:21:37 -0500

    ready for heroku

---
+++ server.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY
+const axios = require('axios')
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+app.use(express.static('public'))
+
+
+
+app.post('/weather', (req, res) => {
+  const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}`
+  console.log(`${req.body.latitude} - latitude`);
+  console.log(`${req.body.longitude} - longitude`)
+  axios({
+      url: url,
+      responseType: 'json'
+  }).then(data => res.json(data.data.currently))
+  // console.log(url);
+})
+
+app.listen(3000, () => {
+  console.log('server has started');
+})
